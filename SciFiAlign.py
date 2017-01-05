da8fbe671e83afc51e7d68af7f10bdb4fcd3478f
Edward Overton <ed@edlap>
2017-01-05 10:47:15 +0000

    Added TOF-cuts

--- SciFiAlign.py
+++ SciFiAlign.py
@@ -8,6 +8,8 @@ using pattern recognition..
 import ROOT
 import libMausCpp  # pylint: disable = W0611
 import math
+import TOFTools
+import os
 
 
 class SciFiAlign:
@@ -37,7 +39,7 @@ class SciFiAlign:
     def fill(self, recon_event):
         """
         Fill the histogram objects with data from the recon event
-        after first checking some cuts..
+        after first checking the tracks
         """
 
         scifi_event = recon_event.GetSciFiEvent()
@@ -99,12 +101,35 @@ class SciFiAlign:
                     histname = "res_%s_%i_%s" % (tk, station, res)
                     profname = "resp_%s_%i_%s" % (tk, station, res)
                     fitname = "resf_%s_%i_%s" % (tk, station, res)
+                    h = getattr(self, histname)
 
-                    prof = getattr(self, histname).ProfileX()
-                    fit = ROOT.TF1("fitname", "pol1", -150, +150)
+                    # silly profile to get mean - poor handling of tails:
+                    #prof = h.ProfileX()
+
+                    # More sophisticated fitter:
+                    NBinsX = h.GetNbinsX()
+                    Min = h.GetMean() - 1.0*h.GetRMS()
+                    Max = h.GetMean() + 1.0*h.GetRMS()
+                    prof = ROOT.TH1F(profname, profname, NBinsX,
+                                     h.GetXaxis().GetBinLowEdge(1),
+                                     h.GetXaxis().GetBinLowEdge(NBinsX+1))
+
+                    for i in range(1, NBinsX+1):
+                        proj = h.ProjectionY("_py", i, i)
+                        if(proj.Integral(proj.FindBin(Min), proj.FindBin(Max))< 3):
+                            continue
+                        result = proj.Fit("gaus", "S", "", Min, Max);
+                        prof.SetBinContent(i, result.Parameter(1));
+                        prof.SetBinError(i, result.Error(1))
+                        #if(MeanUncertainty)out->SetBinError(i, result->Error(1));
+                        #else out->SetBinError(i, result->Parameter(2));
+
+                    fit = ROOT.TF1("fitname", "pol1", -100, +100)
                     prof.Fit(fit, "R")
                     setattr(self, profname, prof)
                     setattr(self, fitname, fit)
+
+
 
     def print_results(self):
         """
@@ -118,8 +143,8 @@ class SciFiAlign:
                 for station in range(1, 6):
                     fit = getattr(self, "resf_%s_%i_%s" % (tk, station, res))
 
-                    print ("%s, %i, %s, %3.4f +/- %3.4f, %3.4f +/- %3.4f" % 
-                           (tk, station, res, fit.GetParameter(1), fit.GetParError(1),
+                    print ("%s, %i, %s, %4.2f +/- %4.2f, %4.3f +/- %4.3f" % 
+                           (tk, station, res, fit.GetParameter(1)*1000, fit.GetParError(1)*1000,
                             fit.GetParameter(0), fit.GetParError(0)))
 
     def draw_result(self, tkname, station, resname):
@@ -142,8 +167,43 @@ class SciFiAlign:
 
 if __name__ == "__main__":
 
-    infiles = ["/home/ed/MICE/data/cooling/08681_recon.root"] # bad 140mev data
+    #infiles = ["/home/ed/MICE/data/08666_recon.root"]  # 170mev
+
+    # bad 140mev data
+    infiles = ["/home/ed/MICE/data/cooling/08681_recon.root"]
+    tof01_low = 28
+    tof01_high = 32
+    
+    # 170mev
+    #infiles = ["/home/ed/MICE/data/08666_recon.root"]
+    #tof01_low = 28
+    #tof01_high = 30.5
+
+    # good 240mev data
+    #infiles = ["/home/ed/MICE/data/08672_recon.root"]
+    #tof01_low = 26.5
+    #tof01_high = 28.5
+    
+    #infiles = ["/home/ed/MICE/data/efficiency_investigation/high_sz_2_6_0.root"]
+    
+    #for MC
+    #inpath = "/home/ed/MICE/data/efficiency_investigation/mc"
+    #infiles = []
+    #for f in os.listdir(inpath):
+    #    if f[5:] == "_sim.root":
+    #        infiles.append(os.path.join(inpath, f))
+    #tof01_low = 28
+    #tof01_high = 32
+    
     max_spills = 0
+    
+    ###########################################################################
+
+    tof_time = ROOT.TH1D("tof01", "tof01", 500, -50, +50)
+
+    kr = [None]*30
+    for i in range (30):
+        kr[i] = ROOT.TH1D("kp_%i"%i, "kp_%i"%i, 100, -5, +5)
 
     chain = ROOT.TChain("Spill")
     for f in infiles:
@@ -167,7 +227,24 @@ if __name__ == "__main__":
 
         for j, recon_event in enumerate(spill.GetReconEvents()):
             print j, ":",
-            align.fill(recon_event)
+               
+            for time in TOFTools.TOF01Times(recon_event.GetTOFEvent().GetTOFEventSpacePoint(), 500):
+                tof_time.Fill(time)
+                
+            # check TOF:
+            if TOFTools.TOF01CoincidenceTime\
+                    (recon_event.GetTOFEvent(), low_ns=tof01_low, 
+                     high_ns=tof01_high):
+
+                # Fill alignment data
+                align.fill(recon_event)
+
+                # Fill kalman data:
+                for kalman_tracks in recon_event.GetSciFiEvent().scifitracks():
+                    for tp in kalman_tracks.scifitrackpoints ():
+                        id = tp.tracker()*15 + (tp.station()-1)*3 + tp.plane()
+                        kr[id].Fill(tp.pull())
+                
 
     align.process()
     align.print_results()
@@ -182,6 +259,21 @@ if __name__ == "__main__":
                     align.draw_result(tk, station, res)
                 
     #align.res_ds_5_x_yres.Draw()
+    #tof_time.Draw()
+    
+    c1 = ROOT.TCanvas("c1", "c1", 800, 600)
+    c1.Divide(5,3)
+    for i in range (15):
+        c1.cd(i+1)
+        kr[i].Draw()
+        
+    c2 = ROOT.TCanvas("c2", "c2", 800, 600)
+    c2.Divide(5,3)
+    for i in range (15):
+        c2.cd(i+1)
+        kr[i+15].Draw()
+        
+    
     raw_input("Done")
             
             
